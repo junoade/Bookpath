4d3654199f1848ff0009ca920e07cd629fb995a6
ChoiJunho <ajchoi0928@gmail.com>
2021-11-11 20:39:01 +0900

    feat: build code

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 const { app, BrowserWindow } = require('electron')
 
-//const path = require('path')
-//const isDev = require('electron-is-dev')
+const path = require('path')
+const isDev = require('electron-is-dev')
 
 require('@electron/remote/main').initialize()
 
@@ -13,11 +13,15 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true,
-      contextIsolation: false
+      //contextIsolation: false
     }
   })
 
-  win.loadURL('http://localhost:3000')
+  win.loadURL(
+    isDev
+    ? 'http://localhost:3000'
+    : `file://${path.join(__dirname, '../build/index.html')}`
+  )
 }
 
 app.on('ready', createWindow)
