8aa0e66cd3e92f3cd30ba80a28f0d02ee014b831
gomgun-lab <64501757+gomgun-lab@users.noreply.github.com>
2021-11-18 18:52:34 +0900

    ui

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import "./Main.css"
+
+function Main(){
+
+  return(
+    <div className = "main">
+      <div className ="updown">
+        <div className ="element_box">
+        </div>
+      </div>
+      <div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
