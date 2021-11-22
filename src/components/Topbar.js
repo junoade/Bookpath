f4148950318d022575449525eebebd68ead1e527
ChoiJunho <ajchoi0928@gmail.com>
2021-11-22 22:05:41 +0900

    Feat: 프론트단 - 백엔드 서버 통신 기능 추가 및 백엔드 API 구현

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,15 +1,53 @@
+import React, { useState, useEffect } from "react"; // useEffect : 앱이 처음 시작되었을 때 useState : 상태 관리 
 import "../css/Topbar.css"
+import axios from "axios"; // axios 라이브러리 이용해서 FE-BE http 통신
 
 function Topbar() {
-  return (  
-  <div>
-    <div className = "header">
-      <p className = "header_name"></p>
+
+  /* */
+  const baseUrl = "http://localhost:8080"
+
+
+  // input에 대한 상태값
+  //const [input, setInput] = useState("");
+
+  // 응답 데이터 관련
+  const [rootDirs, setRootMarks] = useState([]);
+
+
+  // 리액트 앱 실행시 한번만 불러 올 경우, useEffect
+  useEffect(() => {
+    getRootsDir();
+  }, []);
+
+  async function getRootsDir() {
+    await axios.get(baseUrl + "/api/v1/rootDirs=5").then((response => {
+      // 데이터 읽어왔을 때 응답 관련 데이터 상태 관리
+      setRootMarks(response.data);
+      console.log(response.data);
+    })).catch((error) => { console.log(error) }) // http 메소드 실패했을 때 
+  }
+
+
+  return (
+    <div>
+      <div className="header">
+        <p className="header_name"></p>
+      </div>
+      <div className="top_bar">
+        <p><i className="arrow left"></i>
+        {
+          rootDirs
+            ? rootDirs.map((rootDir) => {
+              return (
+                <span>{rootDir.title} </span>
+              )
+            })
+            : null
+        }
+        <i className="arrow right"></i></p>
+      </div>
     </div>
-    <div className ="top_bar">
-     <p><i className ="arrow left"></i>a a a a a a<i className="arrow right"></i></p>
-    </div>
-  </div>
   );
 }
 
