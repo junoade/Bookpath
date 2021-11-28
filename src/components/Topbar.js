97d40e42d53980c0d096a88e2ca787fa4f6ee0a9
gomgun-lab <zxolqs@gmail.com>
2021-11-28 15:37:52 +0900

    UI 수정

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,12 +1,10 @@
-import React, { useState, useEffect } from "react"; // useEffect : 앱이 처음 시작되었을 때 useState : 상태 관리 
-import "../css/Topbar.css"
+import React, { useState, useEffect } from "react"; // useEffect : 앱이 처음 시작되었을 때 useState : 상태 관리
+import "../css/Topbar.css";
 import axios from "axios"; // axios 라이브러리 이용해서 FE-BE http 통신
 
 function Topbar() {
-
   /* */
-  const baseUrl = "http://localhost:8080"
-
+  const baseUrl = "http://localhost:8080";
 
   // input에 대한 상태값
   //const [input, setInput] = useState("");
@@ -14,20 +12,23 @@ function Topbar() {
   // 응답 데이터 관련
   const [rootDirs, setRootMarks] = useState([]);
 
-
   // 리액트 앱 실행시 한번만 불러 올 경우, useEffect
   useEffect(() => {
     getRootsDir();
   }, []);
 
   async function getRootsDir() {
-    await axios.get(baseUrl + "/api/v1/rootDirs=5").then((response => {
-      // 데이터 읽어왔을 때 응답 관련 데이터 상태 관리
-      setRootMarks(response.data);
-      console.log(response.data);
-    })).catch((error) => { console.log(error) }) // http 메소드 실패했을 때 
+    await axios
+      .get(baseUrl + "/api/v1/rootDirs=3")
+      .then((response) => {
+        // 데이터 읽어왔을 때 응답 관련 데이터 상태 관리
+        setRootMarks(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      }); // http 메소드 실패했을 때
   }
-
 
   return (
     <div>
@@ -36,22 +37,21 @@ function Topbar() {
       </div>
       <div className="top_bar">
         <h1>&#8592;</h1>
-        <div className ="folder-box">
-        {
-          rootDirs
-            ? rootDirs.map((rootDir) => {
+
+        {rootDirs
+          ? rootDirs.map((rootDir) => {
               return (
-                <span>{rootDir.title} </span>
-              )
+                <div className="folder-box">
+                  <span className="rootDir_">{rootDir.title}</span>
+                </div>
+              );
             })
-            : null
-        }
-          </div>
+          : null}
+
         <h1>&#8594;</h1>
       </div>
     </div>
   );
 }
-
 
 export default Topbar;
